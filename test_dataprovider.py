6c1bb5dec5e8f2feca44821416d6fd6e51e3eeac
skillrill <team@skillrill.com>
2023-01-12 11:52:36 -0500

    init

---
+++ test_dataprovider.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import pytest
+
+claims = pd.read_csv(r'.\raw_data\sample_claims.csv')
+
+def test_claim_id_is_unique():
+	assert not claims.duplicated(subset=['claim_id']).any()
+	# assert not claims['claim_id'].is_unique
+
+def test_diagnosis_code_is_not_empty():
+	assert not claims['diagnosis_codes'].str.strip().eq('').any()
+
+def test_diagnosis_code_is_not_null():
+	assert not claims['diagnosis_codes'].isnull().values.any()

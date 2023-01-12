a957300337b1d956d4f64d5baa7ed93971266c2a
skillrill <team@skillrill.com>
2023-01-12 11:55:03 -0500

    added req file

--- test_dataprovider.py
+++ test_dataprovider.py
@@ -5,7 +5,6 @@ claims = pd.read_csv(r'.\raw_data\sample_claims.csv')
 
 def test_claim_id_is_unique():
 	assert not claims.duplicated(subset=['claim_id']).any()
-	# assert not claims['claim_id'].is_unique
 
 def test_diagnosis_code_is_not_empty():
 	assert not claims['diagnosis_codes'].str.strip().eq('').any()
